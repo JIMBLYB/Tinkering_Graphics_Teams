8f4be2029c5ae39e96a7c775218a1ecbefcf473f
Joe <jodeph.broughton@gmail.com>
2019-10-21 21:36:42 +0100

    Now the code makes the sprite less green but not any custom colours yet.

--- Contract #4/SpriteReskinning.py
+++ Contract #4/SpriteReskinning.py
@@ -13,12 +13,18 @@ GREEN = (0, 255, 0)
 ORANGE = (255, 165, 0)
 
 
-def make_surface_less_red(surface=my_sprite):
+def make_surface_less_red(surface=pygame.Surface((1, 1))):
     pixel = pygame.Color(0, 0, 0)
     for x in range(surface.get_width()):
         for y in range(surface.get_height()):
-            pygame.PixelArray.surface(my_sprite)
+            pixel = surface.get_at((x, y))
+            surface.set_at(
+                (x, y),
+                pygame.Color(int(pixel.r), int(pixel.g * 0.5), int(pixel.b))
+            )
 
+
+make_surface_less_red(my_sprite)
 
 running = True
 while running:

0585337da4eaa0ddfe74045d345fe8e7323cf75f
Joe <jodeph.broughton@gmail.com>
2019-10-21 21:20:02 +0100

    Pretty basic system that currently only opens the platform sprite I've made.

---
+++ Contract #4/SpriteReskinning.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+
+main = pygame.display.set_mode((400, 200))
+
+my_sprite = pygame.image.load('Platform.png').convert()
+
+# colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+ORANGE = (255, 165, 0)
+
+
+def make_surface_less_red(surface=my_sprite):
+    pixel = pygame.Color(0, 0, 0)
+    for x in range(surface.get_width()):
+        for y in range(surface.get_height()):
+            pygame.PixelArray.surface(my_sprite)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    main.fill(BLACK)
+    main.blit(my_sprite, (0, 0))
+    pygame.display.update()
+
+pygame.quit()

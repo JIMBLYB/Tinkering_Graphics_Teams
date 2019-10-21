ff362c0e792c4daedbd73b40dab1d9f02106cd02
JamesB <git@jamesberry.co.uk>
2019-10-21 23:56:11 +0100

    Test.py is now used as practice for the matplotlib module Simulating Colorblindness.py now using incomplete method to remove gamma

--- Contract #5/Simulating Colorblindness.py
+++ Contract #5/Simulating Colorblindness.py
@@ -1,2 +1,14 @@
+import pygame
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+from skimage import data, img_as_float
+from skimage import exposure
 
+main_window = pygame.display.set_mode((738, 193))
+pic = pygame.image.load("pic.jpg").convert()
+main_window.blit(pic, (0, 0))
+
+img = data.moon()
+gamma_corrected = exposure.adjust_gamma(img,  0, 1)
+main_window.blit(pic, (369, 0))

b86c8ff1d8c72e8299a3264f28e1614e8933a68d
KalvinMalloch <km230683@falmouth.ac.uk>
2019-10-22 08:34:55 +0100

    Added comments and followed PEP 8

--- Contract #2 Kalvin Malloch V2/Contract2.py
+++ Contract #2 Kalvin Malloch V2/Contract2.py
@@ -1,35 +1,16 @@
 import pygame
+import Contract2ModulesLibrary
 
+# Change to Player input in future.
 map_width = 32
 map_height = 16
 cell_size = 32
 
-##brick = pygame.image.load('Stone.png')
-##water = pygame.image.load('Water.png')
-##well = pygame.image.load('Well.png')
-##shrine = pygame.image.load('Shrine.png')
-
-testRed = (200, 0, 0)
-testBlue = (0, 200, 0)
-testGreen = (0, 0, 200)
-
-red = 0
-blue = 1
-green = 2
-
-testBlocks = {
-    red: testRed,
-    blue: testBlue,
-    green: testGreen
-}
-
-tilemap = [
-    [red, blue, green],
-]
-
 pygame.init()
-map_display = pygame.display.set_mode ((map_width, map_height))
+map_display = pygame.display.set_mode((map_width, map_height))
 
+# Looping the game running while drawing the map.
+# Current error: IndexError - list index out of range.
 def map_running():
     map_show = True
     while map_show:
@@ -39,9 +20,11 @@ def map_running():
 
         for row in range(map_height):
             for column in range(map_width):
-                pygame.draw.rect(map_display, testBlocks[tilemap[row][column]], (column * cell_size, row * cell_size, cell_size, cell_size))
+                pygame.draw.rect(map_display, Contract2ModulesLibrary.testBlocks[Contract2ModulesLibrary.tilemap[row]\
+                    [column]], (column * cell_size, row * cell_size, cell_size, cell_size))
         pygame.display.update()
 
     pygame.quit()
+
 
 map_running()

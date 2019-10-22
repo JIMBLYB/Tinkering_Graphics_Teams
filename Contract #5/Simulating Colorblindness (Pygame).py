fd89af13606c424fd21c8ca05083fe009166fe71
JamesB <git@jamesberry.co.uk>
2019-10-22 15:27:10 +0100

    Wrote function for protanopia

--- Contract #5/Simulating Colorblindness (Pygame).py
+++ Contract #5/Simulating Colorblindness (Pygame).py
@@ -1,14 +1,18 @@
 import pygame
+pygame.init()
 
 running = True
-img = pygame.image.load("pic.jpg").convert()
 main_window = pygame.display.set_mode((369, 193))
+img = pygame.image.load("pic.jpg").convert()
+
+# Initialises all the variables for the processed images
+img_protanopia = pygame.image
 
 # Non-colorblind value
 NORMAL = [[100, 0, 0], [0, 100, 0], [0, 0, 100]]
 
 # Red weakness
-PROTANOPIA = [[81.667, 18.333, 0], [55.833, 44.167, 0], [0, 24.167, 75.833]]
+PROTANOPIA = [[56.667, 43.333, 0], [55.833, 44.167, 0], [0, 24.167, 75.833]]
 # Red blind
 PROTANOMALY = [[81.667, 18.333, 0], [33.333, 66.667, 0], [0, 12.5, 87.5]]
 
@@ -29,24 +33,43 @@ ACHROMATANOMALY = [[61.8, 32,    6.2], [16.3, 77.5,  6.2], [16.3, 32.0, 51.6]]
 
 
 # TODO
-# Multiply color values by appropriate matrix (PROTANOPIA)
+# Adjust colour values by appropriate matrix (PROTANOPIA)
 def protanopia():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
             pixel_input = img.get_at((x, y))
 
+            # Gives the RGB values of the pixel their own variables for readability
+            pixel_r = pixel_input[0]
+            pixel_g = pixel_input[1]
+            pixel_b = pixel_input[2]
+
+            pixel_r = int((pixel_r * PROTANOPIA[0][0] / 100) +
+                          (pixel_g * PROTANOPIA[0][1] / 100) +
+                          (pixel_b * PROTANOPIA[0][2] / 100))
+
+            pixel_g = int((pixel_r * PROTANOPIA[1][0] / 100) +
+                          (pixel_g * PROTANOPIA[1][1] / 100) +
+                          (pixel_b * PROTANOPIA[1][2] / 100))
+
+            pixel_b = int((pixel_r * PROTANOPIA[2][0] / 100) +
+                          (pixel_g * PROTANOPIA[2][1] / 100) +
+                          (pixel_b * PROTANOPIA[2][2] / 100))
+
+            img_protanopia.set_at((x, y), (pixel_r, pixel_g, pixel_b))
+
+
 
 # TODO
-# Multiply color values by appropriate matrix (PROTANOMALY)
+# Adjust color values by appropriate matrix (PROTANOMALY)
 def protanomaly():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
             pixel_input = img.get_at((x, y))
-            print(pixel_input)
 
 
 # TODO
-# Multiply color values by appropriate matrix (DEUTARANOPIA)
+# Adjust color values by appropriate matrix (DEUTARANOPIA)
 def deutaranopia():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
@@ -54,7 +77,7 @@ def deutaranopia():
 
 
 # TODO
-# Multiply color values by appropriate matrix (DEUTARANOMALY)
+# Adjust color values by appropriate matrix (DEUTARANOMALY)
 def deutaranomaly():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
@@ -62,7 +85,7 @@ def deutaranomaly():
 
 
 # TODO
-# Multiply color values by appropriate matrix (TRITANOPIA)
+# Adjust color values by appropriate matrix (TRITANOPIA)
 def tritanopia():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
@@ -70,7 +93,7 @@ def tritanopia():
 
 
 # TODO
-# Multiply color values by appropriate matrix (TRITANOMALY)
+# Adjust color values by appropriate matrix (TRITANOMALY)
 def tritanomaly():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
@@ -78,7 +101,7 @@ def tritanomaly():
 
 
 # TODO
-# Multiply color values by appropriate matrix (ACHROMATOPSIA)
+# Adjust color values by appropriate matrix (ACHROMATOPSIA)
 def achromatopsia():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
@@ -86,7 +109,7 @@ def achromatopsia():
 
 
 # TODO
-# Multiply color values by appropriate matrix (ACHROMATANOMALY)
+# Adjust color values by appropriate matrix (ACHROMATANOMALY)
 def achromatanomaly():
     for x in range(img.get_width()):
         for y in range(img.get_height()):
@@ -117,8 +140,7 @@ def image_processing():
 
 # Copies image onto display
 main_window.blit(img, (0, 0))
-protanopia(img)
-
+image_processing()
 
 # Basic loop for exiting program
 # Also updates display every frame

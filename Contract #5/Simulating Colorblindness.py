b76067976ad87341e951f944221a913fb5602728
JIMBLYB <jimblyb2001@gmail.com>
2019-10-22 01:42:48 +0100

    Changed comments to be more concise

--- Contract #5/Simulating Colorblindness.py
+++ Contract #5/Simulating Colorblindness.py
@@ -7,12 +7,14 @@ from skimage import exposure
 
 NEW_GAMMA = 1
 
-# Converts image into an ndarray
+# Converts image into an ndarray to allow matplotlib methods
 img = mpimg.imread("pic.jpg")
 print(img)
 
 # Changes the gamma in the "img" ndarray
 gamma_corrected = exposure.adjust_gamma(img, NEW_GAMMA)
+
+# Shows the image as a plot
 img_plot = plt.imshow(img)
 plt.show()
 

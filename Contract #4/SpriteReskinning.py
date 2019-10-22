0ef0d860dd55d9eb49d487d062995b1af0ccecfb
Joe <jodeph.broughton@gmail.com>
2019-10-22 01:10:04 +0100

    The script will now save the changed sprite as a png

--- Contract #4/SpriteReskinning.py
+++ Contract #4/SpriteReskinning.py
@@ -3,8 +3,10 @@ from pygame.tests.test_utils import png
 
 pygame.init()
 
+# This will create the display that is the same size as my sprite i'm editing.
 main = pygame.display.set_mode((400, 200))
 
+# This surface is the one that actually loads th image.
 my_sprite = pygame.image.load('Platform.png').convert()
 
 # colours
@@ -14,6 +16,7 @@ GREEN = (0, 255, 0)
 ORANGE = (255, 165, 0)
 
 
+# This function will get the x and y pixels and then set them to a different colour.
 def entity_colour_change(surface=pygame.Surface((1, 1))):
     pixel = pygame.Color(0, 0, 0)
     for x in range(surface.get_width()):
@@ -25,9 +28,12 @@ def entity_colour_change(surface=pygame.Surface((1, 1))):
             )
 
 
+# I actually call the function here and put in the sprite surface.
 entity_colour_change(my_sprite)
+# Next i save the new sprite as a png to the project folder
 pygame.image.save(my_sprite, 'main.png')
 
+# This simple game loop will be checking for if the player tries to quit the game and will close the app.
 running = True
 while running:
     for event in pygame.event.get():
@@ -35,7 +41,9 @@ while running:
             running = False
 
     main.fill(WHITE)
+    # Overlays the new sprite over the main background surface.
     main.blit(my_sprite, (0, 0))
+    # Now it will update the display.
     pygame.display.update()
 
 pygame.quit()
